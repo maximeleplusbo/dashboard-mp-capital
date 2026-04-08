2375f8b5ea811b531e6fe8e0d738e592dac61bad
Ferri-Schiavina Romain <fsromain@Mac.lan>
2026-04-08 18:41:06 +0200

    add client info to n8n payload

--- lib/auth0.ts
+++ lib/auth0.ts
@@ -1,4 +1,7 @@
-// lib/auth0.ts
 import { Auth0Client } from '@auth0/nextjs-auth0/server'
 
-export const auth0 = new Auth0Client()
+export const auth0 = new Auth0Client({
+  authorizationParameters: {
+    ui_locales: 'fr'
+  }
+})
