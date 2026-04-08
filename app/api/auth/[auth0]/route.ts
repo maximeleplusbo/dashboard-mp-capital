0299d3a6ec3ce0307d03cc369dbaa8283b1915cf
Ferri-Schiavina Romain <fsromain@Mac.lan>
2026-04-08 14:44:32 +0200

    fix auth0 route handler

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,3 +1,7 @@
 // app/api/auth/[auth0]/route.ts
-import { handlers } from '@auth0/nextjs-auth0'
-export const { GET, POST } = handlers
+import { auth0 } from '@/lib/auth0'
+import { NextRequest } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  return auth0.middleware(request)
+}
